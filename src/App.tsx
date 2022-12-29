2473428107c4b54a63d4c457244651caf1d999e3
Chizycodes <okwudirejoy@gmail.com>
2022-12-29 06:19:54 +0100

    setup pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DashboardLayout from './layout/DashboardLayout';
+import Login from './pages/Login';
+import UserDetail from './pages/users/UserDetail';
+import Users from './pages/users/Users';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/login" element={<Login />} />
+
+				<Route path="/dashboard/*" element={<DashboardLayout />}>
+					<Route index path="users" element={<Users />} />
+					<Route path="user/:id" element={<UserDetail />} />
+				</Route>
+
+				<Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/layout/DashboardLayout.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface Props {}
+
+const DashboardLayout: FC<Props> = () => {
+	return <div>DashboardLayout</div>;
+};
+
+export default DashboardLayout;
