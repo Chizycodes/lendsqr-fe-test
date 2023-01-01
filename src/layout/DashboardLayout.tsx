0fc406c9a485522c7c6a191154dbc4440c69a54f
Chizycodes <okwudirejoy@gmail.com>
2023-01-01 02:52:50 +0100

    dashboard layout final touch

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,21 +1,24 @@
 import { FC, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import SideBar from '../components/dashboard/SideBar';
 import TopNav from '../components/dashboard/TopNav';
 import '../styles/dashboard/dashboard.css';
 
-interface Props {
-	children?: React.ReactNode;
-}
+interface Props {}
 
-const DashboardLayout: FC<Props> = ({ children }) => {
+const DashboardLayout: FC<Props> = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	return (
 		<div className="dashboard-layout">
 			<TopNav setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
-			<SideBar setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
+			<div className='dashboard-container'>
+				<SideBar setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
 
-			<div className="dashboard-main">
-				<div>{children}</div>
+				<div className="dashboard-main">
+					<div>
+						<Outlet />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
