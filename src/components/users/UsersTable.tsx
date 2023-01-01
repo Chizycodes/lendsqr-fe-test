d600032a4e67dce1de19f49fa5979a5ff33c76b0
Chizycodes <okwudirejoy@gmail.com>
2023-01-01 06:18:18 +0100

    Implement user table and pagination

--- src/components/users/UsersTable.tsx
+++ src/components/users/UsersTable.tsx
@@ -1,11 +1,51 @@
 import React, { FC } from 'react';
+import { UserModel } from '../../utils/models';
+import moment from 'moment';
 
 interface Props {
-	users: any;
+	users: UserModel[];
 }
 
+const tableHeaders = ['Organization', 'Username', 'Email', 'Phone number', 'Date joined', 'Status', ''];
+
 const UsersTable: FC<Props> = ({ users }) => {
-	return <div className="users-table"></div>;
+	return (
+		<div className="users-table">
+			<table>
+				<thead>
+					<tr>
+						{tableHeaders.map((header, index) => {
+							return (
+								<th key={index}>
+									<div>
+										<span>{header}</span>
+										{header && <img src="/images/icons/filter-icon.svg" alt="filter-icon" />}
+									</div>
+								</th>
+							);
+						})}
+					</tr>
+				</thead>
+				<tbody>
+					{users.map((user: any, index: number) => {
+						return (
+							<tr key={index}>
+								<td>{user?.orgName}</td>
+								<td>{user?.userName}</td>
+								<td>{user?.email}</td>
+								<td>{user?.phoneNumber}</td>
+								<td>{moment(user?.createdAt).format('MMM D, YYYY h:mm a')}</td>
+								<td>{user?.status}</td>
+								<td>
+									<img src="/images/icons/more-icon.svg" alt="more" />
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default UsersTable;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -3,10 +3,24 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import UsersStats from '../../components/users/UsersStats';
 import UsersTable from '../../components/users/UsersTable';
+import ReactPaginate from 'react-paginate';
 
 const Users = () => {
 	const [loading, setLoading] = useState(false);
 	const [users, setUsers] = useState([]);
+	const [itemOffset, setItemOffset] = useState(0);
+	const itemsPerPage = 10;
+
+	const endOffset = itemOffset + itemsPerPage;
+	const currentItems = users.slice(itemOffset, endOffset);
+	const pageCount = Math.ceil(users.length / itemsPerPage);
+
+	// Invoke when user click to request another page.
+	const handlePageClick = (event: any) => {
+		const newOffset = (event.selected * itemsPerPage) % users.length;
+		console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+		setItemOffset(newOffset);
+	};
 
 	const fetchUsers = async () => {
 		setLoading(true);
@@ -33,7 +47,28 @@ const Users = () => {
 			<h1>Users</h1>
 			<div>
 				<UsersStats />
-				<UsersTable users={users} />
+				<UsersTable users={currentItems} />
+				<div className="users-paginate">
+          <div className='user-page-info'>
+            <p>Showing {itemOffset + 10} out of {users.length}</p>
+          </div>
+					<ReactPaginate
+						nextLabel=">"
+						onPageChange={handlePageClick}
+						pageRangeDisplayed={3}
+						marginPagesDisplayed={2}
+						pageCount={pageCount}
+						previousLabel="<"
+						pageClassName="page-item"
+						pageLinkClassName="page-link"
+						previousClassName="arrow"
+						nextClassName="arrow"
+						breakLabel="..."
+						containerClassName="pagination"
+						activeClassName="active"
+						// renderOnZeroPageCount={null}
+					/>
+				</div>
 			</div>
 		</section>
 	);
